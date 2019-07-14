5aa249032cf6df4021ab4d603f377a8047e1c7c2
Michael Hofmann <michaelhofmann@michaels-mbp.lan>
2019-07-14 13:23:06 -0400

    created game logic

---
+++ js/main.js
@@ -0,0 +1,13 @@
+console.log("Up and Running!");
+//Matching the cards//
+/* Assign variables to designated cards
+Assign cards face images
+Flip the cards over
+If cards match leave them face up
+If cards don't match flip them back */
+var cardOne = "king";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "queen";
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
