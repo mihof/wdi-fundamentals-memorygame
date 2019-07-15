e8f679f43738bc78f49ce3794cc7f4ab89c14e22
Michael Hofmann <michaelhofmann@michaels-mbp.lan>
2019-07-15 04:13:50 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -5,7 +5,29 @@ Assign cards face images
 Flip the cards over
 If cards match leave them face up
 If cards don't match flip them back */
-var cards = ["queen", "queen", "king", "king"];
+const cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+},
+]
+
 var cardsInPlay = [];
 
 function checkForMatch() {
@@ -18,13 +40,18 @@ function checkForMatch() {
 
 function flipCard(cardId1, cardId2) {
 	
-	console.log("User flipped " + cards[cardId1])
-	console.log("User flipped " + cards[cardId2])
-	cardsInPlay.push(cards[cardId1])
-	cardsInPlay.push(cards[cardId2])
+	console.log("User flipped " + cards[cardId1].rank)
+	console.log(cards[cardId1].cardImage)
+	console.log(cards[cardId1].suit)
+	console.log("User flipped " + cards[cardId2].rank)
+	console.log(cards[cardId2].cardImage)
+	console.log(cards[cardId2].suit)
+	cardsInPlay.push(cards[cardId1].rank)
+	cardsInPlay.push(cards[cardId2].rank)
 	checkForMatch()
 	
 }
 
 flipCard(0, 2);
+
 
