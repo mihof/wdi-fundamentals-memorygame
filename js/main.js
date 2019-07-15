6df43b751833faff799a99a1a12ffdce7ae95f73
Michael Hofmann <michaelhofmann@michaels-mbp.lan>
2019-07-15 02:18:29 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -7,17 +7,24 @@ If cards match leave them face up
 If cards don't match flip them back */
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
 
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[2]) {
-		alert("You found a match!");
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
 	} else {
-		alert("Sorry, try again");
+		console.log("Sorry, try again.");
 	}
 }
+
+function flipCard(cardId1, cardId2) {
+	
+	console.log("User flipped " + cards[cardId1])
+	console.log("User flipped " + cards[cardId2])
+	cardsInPlay.push(cards[cardId1])
+	cardsInPlay.push(cards[cardId2])
+	checkForMatch()
+	
+}
+
+flipCard(0, 2);
+
